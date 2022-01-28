6c53ffb3b0478ee31218e09d9e1ea1d9e773a9f4
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-01-28 16:55:36 +0100

    sql

--- eindopdracht/antwoorden.sql
+++ eindopdracht/antwoorden.sql
@@ -1,29 +1,30 @@
 -- Opdracht 1 
-
+SELECT * FROM `videogamesales`;
 -- Opdracht 2 
-
+SELECT * FROM `videogamesales`WHERE year=1999;
 -- Opdracht 3
-
+SELECT SUM(NA_Sales) AS sport_games FROM `videogamesales`WHERE genre='Sports';
 -- Opdracht 4
-
+SELECT name FROM `videogamesales` WHERE publisher = "Nintendo" AND year >= 1990 AND year <= 2005;
 -- Opdracht 5
-
+SELECT MAX(Global_Sales) , name AS Meeste_Sales_Wereldwijd FROM `videogamesales`;
 -- Opdracht 6 
-
+SELECT AVG(EU_Sales) FROM `videogamesales` WHERE genre = 'puzzle'
 -- Opdracht 7 
-
+SELECT name, genre, publisher FROM `videogamesales` WHERE JP_Sales= 532;
 -- Opdracht 8
-
+SELECT SUM(Global_Sales) AS wereldwijde_sales FROM `videogamesales` WHERE publisher="Nintendo";
 -- Opdracht 9
-
+SELECT name, year FROM `videogamesales` WHERE genre = 'racing' AND (publisher= "nintendo" OR publisher = "activision")
 -- Opdracht 10
-
+SELECT AVG(`JP_Sales`) AS average_sales_jp, AVG(`NA_Sales`) AS na_average_sales, AVG(`EU_Sales`) AS average_sales_eu FROM `videogamesales`;
 -- Opdracht 11
-
+DELETE  FROM `videogamesales` WHERE name="halo 2";
 -- Opdracht 12
-
+DELETE  FROM `videogamesales` WHERE year = 2012 AND publisher = "ubisoft"
 -- Opdracht 13
-
+DELETE  FROM `videogamesales` WHERE genre = "Adventure"  AND publisher = "activison"
 -- Opdracht 14
-
+DELETE  FROM `videogamesales` WHERE Global_Sales < 10000 AND publisher = "Nintendo";
 -- Opdracht 15
+DELETE FROM `videogamesales` WHERE year = 1997 AND NA_Sales >= 200000; 
